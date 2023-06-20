01a4a5da39f1ff0c0582fc0690f76f6b8d09f661
Sandeep kumar singh <sandeeprj793@gmail.com>
2023-06-20 21:27:37 +0530

    [Sandeep] Add: Percentage head vs tail

---
+++ Functional_Programming/Functional_Programming/CoinFlip.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Functional_Programming
+{
+    public class CoinFlip
+    {
+        private int headsCount;
+        private int tailsCount;
+        private Random random;
+
+        public CoinFlip()
+        {
+            random = new Random();
+            headsCount = 0;
+            tailsCount = 0;
+        }
+
+        public void PerformFlips(int numberOfFlips)
+        {
+            for (int i = 0; i < numberOfFlips; i++)
+            {
+                double randomValue = random.NextDouble();
+
+                if (randomValue < 0.5)
+                    tailsCount++;
+                else
+                    headsCount++;
+            }
+        }
+
+        public void DisplayResults()
+        {
+            double headsPercentage = (double)headsCount / (headsCount + tailsCount) * 100;
+            double tailsPercentage = (double)tailsCount / (headsCount + tailsCount) * 100;
+
+            Console.WriteLine("Heads: {0} ({1:F2}%)", headsCount, headsPercentage);
+            Console.WriteLine("Tails: {0} ({1:F2}%)", tailsCount, tailsPercentage);
+        }
+    }
+}
---
+++ Functional_Programming/Functional_Programming/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Functional_Programming
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Functional programming problem");
+            Console.WriteLine("Please choose 1program from below option");
+            Console.WriteLine("1.FlipCoin");
+            int option = Convert.ToInt32(Console.ReadLine());
+
+            switch(option)
+            {
+                case 1:
+                    Console.Write("Enter the number of times to flip the coin: ");
+                    int numberOfFlips = Convert.ToInt32(Console.ReadLine());
+
+                    //Validate the input to ensure it is a positive integer
+                    if (numberOfFlips < 0)
+                    {
+                        Console.WriteLine("Invalid input. Please enter a positive integer.");
+                    }
+
+                    CoinFlip coinFlip = new CoinFlip();
+                    coinFlip.PerformFlips(numberOfFlips);
+                    coinFlip.DisplayResults();
+                    break;
+                default:
+                    Console.WriteLine("please choose program with given option");
+                    break;
+            }
+            Console.ReadLine();
+        }
+    }
+}
